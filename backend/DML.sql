75143487d3024b7dbb768e7c1747e423638d62d8
TranHai <hairobet15092005@gmail.com>
2024-09-25 20:43:11 +0700

    sql

--- backend/DML.sql
+++ backend/DML.sql
@@ -19,6 +19,23 @@
 CREATE DATABASE IF NOT EXISTS `pcb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `pcb`;
 
+-- Dumping structure for table pcb.mach_dien
+CREATE TABLE IF NOT EXISTS `mach_dien` (
+  `id_mach` int NOT NULL AUTO_INCREMENT,
+  `anh_mach` varchar(255) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `so_loi` int DEFAULT NULL,
+  `bao_cao` text COLLATE utf8mb4_vietnamese_ci,
+  `ngay_them` datetime DEFAULT CURRENT_TIMESTAMP,
+  `id` int DEFAULT NULL,
+  PRIMARY KEY (`id_mach`),
+  KEY `id` (`id`),
+  CONSTRAINT `mach_dien_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- Dumping data for table pcb.mach_dien: ~0 rows (approximately)
+INSERT INTO `mach_dien` (`id_mach`, `anh_mach`, `so_loi`, `bao_cao`, `ngay_them`, `id`) VALUES
+	(8, 'mach_dien_01.png', 2, 'hoan thanh tot', '2024-09-23 00:00:00', 4);
+
 -- Dumping structure for table pcb.user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -30,19 +47,13 @@ CREATE TABLE IF NOT EXISTS `user` (
   `is_admin` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
 -- Dumping data for table pcb.user: ~9 rows (approximately)
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `provider`, `created_at`, `is_admin`) VALUES
 	(4, 'tttttttt', 'hairobet15092005@gmail.com', '$2a$10$cme5WDoJ1JOaoyRcHHOwC.9dYEuoa0Sp7g/XVelrik05UNA/2091K', 'email', '2024-09-23 02:50:00', 1),
 	(6, 'Tran van Hai', 'tranvanhai12345@gmail.com', '$2a$10$1s3EmDKhWekhSuhU.S/n.O9KV3e/Mo2f5gBiC23E8C8JQdK9VeJAq', 'email', '2024-09-23 07:33:37', 0),
-	(7, 'j365030@gmail.com', 'hairobet150920r05@gmail.com', '$2a$10$c59b8H.FcmLR6Ax9v8bzVe9SUtrglcI0HaGXnyHlv.5eTNwNIeVbm', 'email', '2024-09-23 07:33:43', 0),
-	(8, 'matheobe ', 'hairobet92005@gmail.com', '$2a$10$kt7hDHT6vzUKCliBah1rOOwtjPIGOh.bhyqU/vpjr9XPA3LH.6nRK', 'email', '2024-09-23 08:45:23', 0),
-	(9, 'hairobet15092005@gmail.com', 'hairobet15ee092005@gmail.com', '$2a$10$o/ezZSFcmcYUq.s27.eGlOAEeQonhFRVv6f4oKiV6X8ReZYaP5CAG', 'email', '2024-09-23 09:37:15', 0),
-	(10, 'Tran van Hai', 'tranvanhai12uu345@gmail.com', '$2a$10$51BnkFS1VKjkb1pxxwnmaOtm1FCAUM9M..MEjXs9Le80bgfpZrD0q', 'email', '2024-09-23 11:18:44', 0),
-	(11, 'jjjjhsjgbvjkba', 'hairobet5@gmail.com', '$2a$10$7npibaNwoMsRxQxyJWA4QuKipXDGvJuyXgE5dhnF8qIo8aEDRUEnG', 'email', '2024-09-23 13:59:47', 0),
-	(12, 'dddddddd', 'hai@gmail.com', '$2a$10$GpBQba.4yuLVEGRJhKslKuZDiS9qEg4Efr/00dPYWgepO7.sFLYqG', 'email', '2024-09-23 14:00:11', 0),
-	(13, 'j365030@gmail.com', 'hai123@gmail.com', '$2a$10$QuZWcDIxhbCXdyQ34HhEOuaIe/NiBno/DQwQ2R9.4lLpuS5nkc7nW', 'email', '2024-09-23 14:10:13', 0);
+	(7, 'j365030@gmail.com', 'hairobet150920r05@gmail.com', '$2a$10$c59b8H.FcmLR6Ax9v8bzVe9SUtrglcI0HaGXnyHlv.5eTNwNIeVbm', 'email', '2024-09-23 07:33:43', 0);
 
 -- Dumping structure for table pcb.user_sessions
 CREATE TABLE IF NOT EXISTS `user_sessions` (
@@ -55,15 +66,11 @@ CREATE TABLE IF NOT EXISTS `user_sessions` (
   PRIMARY KEY (`session_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `user_sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
--- Dumping data for table pcb.user_sessions: ~3 rows (approximately)
+-- Dumping data for table pcb.user_sessions: ~1 rows (approximately)
 INSERT INTO `user_sessions` (`session_id`, `user_id`, `access_token`, `refresh_token`, `created_at`, `expires_at`) VALUES
-	(33, 13, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMsImlzX2FkbWluIjowLCJpYXQiOjE3MjcxMDA2MjMsImV4cCI6MTcyNzEwMDc0M30.Y5To6iJfCI_fMcHrpHD7MBJLaH26TvhwRDbFnxf2WvY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMsImlzX2FkbWluIjowLCJpYXQiOjE3MjcxMDA2MjMsImV4cCI6MTc1ODYzNjYyM30.qpuWWKo2lxXgQJOwe6XGBuTvgqMvuY8yzvYbptYKoZE', '2024-09-23 14:10:23', '2025-09-23 14:10:24'),
-	(34, 4, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzEwMDk4NywiZXhwIjoxNzI3MTAxMTA3fQ.TvUx0nUkGyz3pG2LnaiZc-uIVAL38LIW6w2HN-2bwEo', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzEwMjk1NywiZXhwIjoxNzU4NjM4OTU3fQ.YHgKhzDCmvaD0_cqK7NxsbBUjTyb-B-Es7MzTilEwbM', '2024-09-23 14:16:27', '2025-09-23 14:16:27'),
-	(35, 4, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzEwMjQ3NiwiZXhwIjoxNzI3MTAyNTk2fQ.6ocGakZIRvAuX682gcn_76v2zFu9g3qAG7N52eVRjbw', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzEwMjk1NywiZXhwIjoxNzU4NjM4OTU3fQ.YHgKhzDCmvaD0_cqK7NxsbBUjTyb-B-Es7MzTilEwbM', '2024-09-23 14:41:16', '2025-09-23 14:41:17'),
-	(36, 4, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzEwMjkyNCwiZXhwIjoxNzI3MTAzMDQ0fQ.zH5zgc8u2MlHPTAhBEdY3pjahz35u0Tx1WmEeSEgxm4', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzEwMjk1NywiZXhwIjoxNzU4NjM4OTU3fQ.YHgKhzDCmvaD0_cqK7NxsbBUjTyb-B-Es7MzTilEwbM', '2024-09-23 14:48:44', '2025-09-23 14:48:44'),
-	(37, 4, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzEwMjk0MSwiZXhwIjoxNzI3MTAzMDYxfQ.LWezaptfA40ZKojxWv3DCpLiBpKpeDuGd9ZLAj2hevo', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzEwMjk1NywiZXhwIjoxNzU4NjM4OTU3fQ.YHgKhzDCmvaD0_cqK7NxsbBUjTyb-B-Es7MzTilEwbM', '2024-09-23 14:49:01', '2025-09-23 14:49:01');
+	(69, 4, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzE5NzE3NCwiZXhwIjoxNzI3MTk3MjM0fQ.fcIhr0cqpCpoJDh3FNkwxOuQstsH9M-t9J4EdAiV_Tk', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaXNfYWRtaW4iOjEsImlhdCI6MTcyNzE5NzE3NCwiZXhwIjoxNzU4NzMzMTc0fQ.VHflzjL4mGBtc7RvyK0Ixg-bMgwhM6mDZL2Pl1-JF44', '2024-09-24 16:59:34', '2025-09-24 16:59:34');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
